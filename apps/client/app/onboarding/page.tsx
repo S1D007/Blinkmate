9740dbfa3925efe48cde72a58e3f301f65f8d12c
Siddhant <s1d.official011001011@gmail.com>
2024-09-02 02:27:52 +0530

    feat: added router and changed animation duration

--- apps/client/app/onboarding/page.tsx
+++ apps/client/app/onboarding/page.tsx
@@ -3,15 +3,23 @@
 import React, { useEffect, useState } from 'react';
 import AnimatedBeamComponent from './_components/AnimatedBeamComponent';
 import AnimatedNotificationComponent from './_components/AnimatedNotificationComponent';
+import { useRouter } from 'next/navigation';
 
 const COMPONENTS = [<AnimatedNotificationComponent />, <AnimatedBeamComponent />];
 
 function page() {
   const [currentComponent, setCurrentComponent] = useState(0);
+  const router = useRouter();
   useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentComponent(prev => (prev + 1) % COMPONENTS.length);
-    }, 6000);
+    const interval = setInterval(
+      () => {
+        setCurrentComponent(prev => prev + 1);
+        if (currentComponent === COMPONENTS.length - 1) {
+          router.push('auth');
+        }
+      },
+      currentComponent === 0 ? 6000 : 3000,
+    );
     return () => clearInterval(interval);
   });
 
