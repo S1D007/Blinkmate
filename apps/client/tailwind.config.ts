1b1bfd094e08fab9545256a82aaedbfe6c5f214b
Siddhant <s1d.official011001011@gmail.com>
2024-09-01 11:00:10 +0530

    chore: misc changes

--- apps/client/tailwind.config.ts
+++ apps/client/tailwind.config.ts
@@ -8,9 +8,18 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      animation: {
+        ripple: 'ripple var(--duration,2s) ease calc(var(--i, 0)*.2s) infinite',
+      },
+      keyframes: {
+        ripple: {
+          '0%, 100%': {
+            transform: 'translate(-50%, -50%) scale(1)',
+          },
+          '50%': {
+            transform: 'translate(-50%, -50%) scale(0.9)',
+          },
+        },
       },
     },
   },
